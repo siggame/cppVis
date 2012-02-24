14087949fd45d9200e1debe77498db2d4d6145f5
Jacob Gardner <jacob.v.gardner@gmail.com>
2012-02-24 00:39:18 -0600

    Log size output

--- lib/gui/gui.cpp
+++ lib/gui/gui.cpp
@@ -122,6 +122,7 @@ namespace visualizer
     if( !parserFound )
     {
       WARNING( "An appropriate game player could not be found!" );
+      cout << "Log Size: " << fullLog.size() << endl;
       cout << fullLog << endl;
     }
   }
