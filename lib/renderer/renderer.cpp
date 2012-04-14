9b4946d3520888050fdebb8752e7b1b11a7ee5cf
Jacob Gardner <jacob.v.gardner@gmail.com>
2012-04-14 17:44:00 -0500

    FBO stuff

--- lib/renderer/renderer.cpp
+++ lib/renderer/renderer.cpp
@@ -121,7 +121,11 @@ namespace visualizer
       update( TimeManager->getTurn(), 0 );
     }
 
-    glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+    if( fboSupport() )
+    {
+      glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+    }
+
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
     if( fboSupport() )
