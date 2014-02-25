b69519faea34a0d4afcc5ba89a3ce1e2c9c9569d
Bryce Mehring <bm4g5@mst.edu>
2014-02-24 21:33:51 -0600

    Added the ability to statically link chess

--- lib/games/games.cpp
+++ lib/games/games.cpp
@@ -5,6 +5,10 @@ using namespace std;
 #include <CoreFoundation/CoreFoundation.h>
 #endif
 
+#ifdef STATIC_BUILD
+#include "../chess/visualizer/chess.h"
+#endif // STATIC_BUILD
+
 namespace visualizer
 {
 
@@ -12,91 +16,118 @@ namespace visualizer
 
   void _Games::setup()
   {
-    if( !Games )
-    {
-      Games = new _Games;
-    }
+	if( !Games )
+	{
+	  Games = new _Games;
+	}
 
-    Games->_setup();
+	Games->_setup();
   } // _Games::setup()
 
   void _Games::_setup()
   {
-    MESSAGE( "============Setting Up Plugins=======" );
-    IGame *game = 0;
+		MESSAGE( "============Setting Up Plugins=======" );
 
-    QDir pluginsDir( qApp->applicationDirPath() );
-    QStringList pluginFilter;
+#ifdef STATIC_BUILD
+		if(!m_gameList.empty())
+			return;
 
-    pluginFilter << "*.dll" << "*.so" << "*.dylib";
-    pluginsDir.setNameFilters(pluginFilter);
+		Chess* theGame = new Chess;
 
-    pluginsDir.cd( "plugins" );
-    foreach( QString fileName, pluginsDir.entryList( QDir::Files ) )
-    {
-      QPluginLoader& pluginLoader = *new QPluginLoader( pluginsDir.absoluteFilePath( fileName ) );
-      m_plugins.push_back( &pluginLoader );
-      //QPluginLoader pluginLoader( pluginsDir.absoluteFilePath( fileName ) );
-      QObject *plugin = pluginLoader.instance();
-      if( plugin )
-      {
-        game = qobject_cast<IGame *>( plugin );
-        if( game )
-        {
-#if __DEBUG__
-          cerr << "Plugin Loaded: " << qPrintable( pluginsDir.absoluteFilePath( fileName ) ) << endl;
-#endif
-          m_gameList.push_back( game );
-          game->gui = GUI;
-          game->animationEngine = AnimationEngine;
-          game->options = OptionsMan;
-          game->renderer = Renderer;
-          game->resourceManager = ResourceMan;
-          game->textureLoader = TextureLoader;
-          game->timeManager = TimeManager;
-          game->errorLog = errorLog;
-        } 
-        else
-        {
-          MESSAGE( "Plugin is not valid: %s", qPrintable( pluginsDir.absoluteFilePath( fileName ) ) );
-          WARNING( "Plugin couldn't load. Reason: \n%s", qPrintable( pluginLoader.errorString() ) );
-        }
-      }
-      else
-      {
-        MESSAGE( "Plugin could not be loaded into memory. %s", qPrintable( pluginsDir.absoluteFilePath( fileName ) ) );
-        WARNING( "Plugin couldn't load. Reason: \n%s", qPrintable( pluginLoader.errorString() ) );
+		theGame->gui = GUI;
+		theGame->animationEngine = AnimationEngine;
+		theGame->options = OptionsMan;
+		theGame->renderer = Renderer;
+		theGame->resourceManager = ResourceMan;
+		theGame->textureLoader = TextureLoader;
+		theGame->timeManager = TimeManager;
+		theGame->errorLog = errorLog;
 
-      }
-    }
-    MESSAGE( "============Plugins Are Initialized=======" );
-    
-  } // _Games::_setup()
+		m_gameList.push_back( theGame );
+#else
+		IGame *game = 0;
+
+		QDir pluginsDir( qApp->applicationDirPath() );
+		QStringList pluginFilter;
+
+		pluginFilter << "*.dll" << "*.so" << "*.dylib";
+		pluginsDir.setNameFilters(pluginFilter);
+
+		pluginsDir.cd( "plugins" );
+		foreach( QString fileName, pluginsDir.entryList( QDir::Files ) )
+		{
+			QPluginLoader& pluginLoader = *new QPluginLoader( pluginsDir.absoluteFilePath( fileName ) );
+			m_plugins.push_back( &pluginLoader );
+			//QPluginLoader pluginLoader( pluginsDir.absoluteFilePath( fileName ) );
+			QObject *plugin = pluginLoader.instance();
+			if( plugin )
+			{
+				game = qobject_cast<IGame *>( plugin );
+				if( game )
+				{
+					#if __DEBUG__
+					cerr << "Plugin Loaded: " << qPrintable( pluginsDir.absoluteFilePath( fileName ) ) << endl;
+					#endif
+					m_gameList.push_back( game );
+					game->gui = GUI;
+					game->animationEngine = AnimationEngine;
+					game->options = OptionsMan;
+					game->renderer = Renderer;
+					game->resourceManager = ResourceMan;
+					game->textureLoader = TextureLoader;
+					game->timeManager = TimeManager;
+					game->errorLog = errorLog;
+				}
+				else
+				{
+				MESSAGE( "Plugin is not valid: %s", qPrintable( pluginsDir.absoluteFilePath( fileName ) ) );
+				WARNING( "Plugin couldn't load. Reason: \n%s", qPrintable( pluginLoader.errorString() ) );
+				}
+			}
+			else
+			{
+			MESSAGE( "Plugin could not be loaded into memory. %s", qPrintable( pluginsDir.absoluteFilePath( fileName ) ) );
+			WARNING( "Plugin couldn't load. Reason: \n%s", qPrintable( pluginLoader.errorString() ) );
+
+			}
+		}
+#endif // STATIC_BUILD
+		 MESSAGE( "============Plugins Are Initialized=======" );
+	} // _Games::_setup()
 
   std::vector< IGame* >& _Games::gameList()
   {
-    return m_gameList;
+	return m_gameList;
   } // _Games::gameList()
 
   void _Games::destroy()
   {
-    delete Games;
-    Games = 0;
+	delete Games;
+	Games = 0;
 
   } // _Games::destroy()
 
   _Games::~_Games()
   {
-    for
-      ( 
-      vector< QPluginLoader* >::iterator i = m_plugins.begin();
-      i != m_plugins.end();
-      i++ 
-      )
-    {
-      (*i)->unload();
-      delete *i;
-    }
+#ifdef STATIC_BUILD
+	  if(!m_gameList.empty())
+	  {
+		  delete m_gameList[0];
+	  }
+#else
+	for
+	  (
+	  vector< QPluginLoader* >::iterator i = m_plugins.begin();
+	  i != m_plugins.end();
+	  i++
+	  )
+	{
+	  (*i)->unload();
+	  delete *i;
+	}
+#endif // STATIC_BUILD
+
+
 
   }
 
